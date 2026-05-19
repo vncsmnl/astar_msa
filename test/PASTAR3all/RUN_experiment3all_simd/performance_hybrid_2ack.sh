02504c448759f4e29d6e3ed52723ca3acc9dde3f
Daniel Sundfeld <daniel.sundfeld@gmail.com>
2026-05-19 08:12:18 -0300

    3allsimd experiments

---
+++ test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_2ack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SEQ="./seqs/Balibase/Ref1/7_long_medium_id/2ack.fasta"
+THREADS="-t 16"
+
+#Affinity for Intel 13700kf
+AFFINITY="--affinity=0,2,4,6,8,10,12,14,16,17,18,19,20,21,22,23"
+HYBRID="8,9,8,7"
+echo $HYBRID
+/usr/bin/time -v perf stat -r 3 ./bin/msa_pastar ${THREADS} ${AFFINITY} --hybrid=${HYBRID} $SEQ >> ${SEQ}.map.${HYBRID}.out 2>&1
