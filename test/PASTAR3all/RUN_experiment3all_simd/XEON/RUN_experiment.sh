103c965c3ad2a12d8a06ff54f6c2e19fee532044
Vinícius Manoel  <59481808+vncsmnl@users.noreply.github.com>
2026-05-20 14:07:13 -0300

    feat: add hybrid and Xeon performance scripts for various sequences

---
+++ test/PASTAR3all/RUN_experiment3all_simd/XEON/RUN_experiment.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+./test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_1aboA.sh
+./test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_actin.sh
+./test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_2ack.sh
+./test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_2cba.sh
+./test/PASTAR3all/RUN_experiment3all_simd/performance_hybrid_1sesA.sh
---
+++ test/PASTAR3all/RUN_experiment3all_simd/XEON/performance_xeon_2ack.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+SEQ="./seqs/Balibase/Ref1/7_long_medium_id/2ack.fasta"
+THREADS="-t 14"
+
+# affinity for 14 threads, to avoid hyperthreading
+AFFINITY="--affinity=0,2,4,6,8,10,12,14,16,18,20,22,24,26"
+
+echo "Xeon E5-2680 v4"
+/usr/bin/time -v perf stat -r 3 ./bin/msa_pastar ${THREADS} ${AFFINITY} $SEQ >> ${SEQ}.xeon.out 2>&1
