9e500a1d10dfa4818dda0e29a16222ee1354b2f4
Vinicius Manoel <viniciusmanoel@duck.com>
2026-05-20 20:29:18 -0300

    Update performance scripts to support multiple binary suffixes for benchmarking

--- test/PASTAR3all/RUN_experiment3all_simd/XEON/performance_xeon_1sesA.sh
+++ test/PASTAR3all/RUN_experiment3all_simd/XEON/performance_xeon_1sesA.sh
@@ -19,14 +19,15 @@ echo "----------------------------------------"
 run_test () {
     MODE=$1
     AFFINITY_ARG=$2
-    OUT_FILE="${OUT_DIR}/${SEQ_NAME}.${MODE}.out"
+    BIN_SUFFIX=$3
+    OUT_FILE="${OUT_DIR}/${SEQ_NAME}.${BIN_SUFFIX}.${MODE}.out"
 
     echo ""
     echo ">>> Rodando modo: $MODE"
     echo "Saída: $OUT_FILE"
 
     /usr/bin/time -v perf stat -r 3 \
-        "${ROOT_DIR}/bin/msa_pastar_simd" \
+        "${ROOT_DIR}/bin/msa_pastar_${BIN_SUFFIX}" \
         ${THREADS} ${AFFINITY_ARG} "$SEQ" \
         >> "$OUT_FILE" 2>&1
 }
@@ -34,14 +35,18 @@ run_test () {
 # ---------------------------
 # 1) Without affinity
 # ---------------------------
-run_test "no_affinity" ""
+for BIN_SUFFIX in 2all 3all simd; do
+    run_test "no_affinity" "" "$BIN_SUFFIX"
+done
 
 # ---------------------------
 # 2) With affinity
 # (physical cores)
 # ---------------------------
 AFFINITY="--affinity=0,2,4,6,8,10,12,14,16,18,20,22,24,26"
-run_test "affinity" "$AFFINITY"
+for BIN_SUFFIX in 2all 3all simd; do
+    run_test "affinity" "$AFFINITY" "$BIN_SUFFIX"
+done
 
 echo ""
 echo "Benchmark finalizado."
